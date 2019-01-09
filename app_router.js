ec5dc15ac73d78549ca4058e8ff54703c5ba3af7
James Kung <kong1191@gmail.com>
2019-01-10 01:43:48 +0800

    Handle line join event

--- app_router.js
+++ app_router.js
@@ -13,20 +13,30 @@ const linebotParser = bot.parser();
 // Redirect linebot webhook url to linebot parser
 app.post('/linewebhook', linebotParser);
 
-bot.on('message', function (event) {
+const join_message = '謝謝邀請我加入群組，讓我有機會服務大家～';
+
+bot.on('join', function(event) {
+    event.reply(join_message).then(function(data) {
+        console.debug('Success to reply join event');
+    }).catch(function(error) {
+        console.error('Failed to reply join event', error);
+    });
+});
+
+bot.on('message', function(event) {
     event.reply(event.message.text).then(function (data) {
-        console.log('Success', data);
-    }).catch(function (error) {
-        console.log('Error', error);
+        console.debug('Success to reply message event', data);
+    }).catch(function(error) {
+        console.error('Failed to reply message event', error);
     });
 });
 
 //Express API --------- App.get('path', callback function);
 //routes HTTP GET requests to the specified path with the specified callback functions
-app.get('/', function (request, response) {
+app.get('/', function(request, response) {
     response.json({ message: 'response from node service!' });
 });
 
-app.post('/ajax', function (request, response) {
+app.post('/ajax', function(request, response) {
     response.send("response by ajax");
 });
